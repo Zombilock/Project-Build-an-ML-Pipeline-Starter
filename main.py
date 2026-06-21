86271e84e97b03c7da7cf39037d211ae6b732826
Christopher Clark <christopherjamesclark34@gmail.com>
2026-06-21 00:01:07 -0400

    Final fix for data check

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ def go(config: DictConfig):
             parameters={
                 "csv": "clean_sample.csv:latest",
                 "ref": "clean_sample.csv:reference",
-                "reference_artifact": "clean_sample.csv:latest",
                 "kl_threshold": config["data_check"]["kl_threshold"],
                 "min_price": config["etl"]["min_price"],
                 "max_price": config["etl"]["max_price"]
